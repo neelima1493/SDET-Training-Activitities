7c9e99229b400cf3b9860672635265cc5c07112b
neelima1493 <70313098+neelima1493@users.noreply.github.com>
2020-10-28 11:28:48 -0500

    python activities

---
+++ PYTHON/Activity_1.py
@@ -0,0 +1,4 @@
+Name = input("Enter your Name: ")
+Age = int(input("Enter your age: "))
+year = str((2020 - Age)+100)
+print(Name + " will turn 100 years old in " + year)
---
+++ PYTHON/Activity_2.py
@@ -0,0 +1,6 @@
+Num = int(input("Please enter an Integer Number: "))
+mod = Num % 2
+if mod>0:
+    print("Entered Number is Odd number.")
+else:
+    print("Entered Number is even number.")
